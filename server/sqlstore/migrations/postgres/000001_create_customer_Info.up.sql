d28d3aaa18b21948f17a09577e637c028debdfd0
coltoneshaw <colton@theshaw.group>
2023-12-26 12:43:46 -0500

    Fixed another migration issue

--- server/sqlstore/migrations/postgres/000001_create_customer_Info.up.sql
+++ server/sqlstore/migrations/postgres/000001_create_customer_Info.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS crm_customers (
-	INSTEAD TEXT PRIMARY KEY,
+	ID TEXT PRIMARY KEY,
 	name TEXT NOT NULL,
 	customerSuccessManager TEXT NOT NULL,
-	accountExecutive TEXTm NOT NULL,
+	accountExecutive TEXT NOT NULL,
 	technicalAccountManager TEXT NOT NULL,
 	salesforceId TEXT,
 	zendeskId TEXT,
-	licensedTo TEXT
+	licensedTo TEXT,
 	siteUrl TEXT,
 	type TEXT NOT NULL
 );
