efcb2e3bdd0c69d2e3ea2247bb3bf5be2569b3b1
coltoneshaw <colton@theshaw.group>
2023-12-27 00:01:56 -0500

    Added additional perm functions from playbooks

--- server/app/permissions_service.go
+++ server/app/permissions_service.go
@@ -1,5 +1,29 @@
 package app
 
+import (
+	"github.com/mattermost/mattermost/server/public/model"
+	pluginapi "github.com/mattermost/mattermost/server/public/pluginapi"
+	"github.com/pkg/errors"
+)
+
+// IsSystemAdmin returns true if the userID is a system admin
+func IsSystemAdmin(userID string, pluginAPI *pluginapi.Client) bool {
+	return pluginAPI.User.HasPermissionTo(userID, model.PermissionManageSystem)
+}
+
+func CanPostToChannel(userID, channelID string, pluginAPI *pluginapi.Client) bool {
+	return pluginAPI.User.HasPermissionToChannel(userID, channelID, model.PermissionCreatePost)
+}
+
+func IsMemberOfTeam(userID, teamID string, pluginAPI *pluginapi.Client) bool {
+	teamMember, err := pluginAPI.Team.GetMember(teamID, userID)
+	if err != nil {
+		return false
+	}
+
+	return teamMember.DeleteAt == 0
+}
+
 // RequesterInfo holds the userID and teamID that this request is regarding, and permissions
 // for the user making the request
 type RequesterInfo struct {
@@ -7,4 +31,29 @@ type RequesterInfo struct {
 	TeamID  string
 	IsAdmin bool
 	IsGuest bool
+}
+
+// IsGuest returns true if the userID is a system guest
+func IsGuest(userID string, pluginAPI *pluginapi.Client) (bool, error) {
+	user, err := pluginAPI.User.Get(userID)
+	if err != nil {
+		return false, errors.Wrapf(err, "Unable to get user to determine permissions, user id `%s`", userID)
+	}
+
+	return user.IsGuest(), nil
+}
+
+func GetRequesterInfo(userID string, pluginAPI *pluginapi.Client) (RequesterInfo, error) {
+	isAdmin := IsSystemAdmin(userID, pluginAPI)
+
+	isGuest, err := IsGuest(userID, pluginAPI)
+	if err != nil {
+		return RequesterInfo{}, err
+	}
+
+	return RequesterInfo{
+		UserID:  userID,
+		IsAdmin: isAdmin,
+		IsGuest: isGuest,
+	}, nil
 }
