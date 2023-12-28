7b68103834cf17f4f884dc38878cd5c4dccf74c6
coltoneshaw <colton@theshaw.group>
2023-12-27 21:52:05 -0500

    added mocks

--- build/custom.mk
+++ build/custom.mk
@@ -3,9 +3,8 @@
 ## Generate mocks.
 mocks:
 ifneq ($(HAS_SERVER),)
-	mockgen -destination server/bot/mocks/mock_poster.go github.com/coltoneshaw/mattermost-plugin-customers/server/bot Poster
-	mockgen -destination server/app/mocks/mock_job_once_scheduler.go github.com/coltoneshaw/mattermost-plugin-customers/server/app JobOnceScheduler
-	mockgen -destination server/sqlstore/mocks/mock_kvapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore KVAPI
-	mockgen -destination server/sqlstore/mocks/mock_storeapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore StoreAPI
-	mockgen -destination server/sqlstore/mocks/mock_configurationapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore ConfigurationAPI
+	./bin/mockgen -destination server/bot/mocks/mock_poster.go github.com/coltoneshaw/mattermost-plugin-customers/server/bot Poster
+	./bin/mockgen -destination server/sqlstore/mocks/mock_kvapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore KVAPI
+	./bin/mockgen -destination server/sqlstore/mocks/mock_storeapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore StoreAPI
+	./bin/mockgen -destination server/sqlstore/mocks/mock_configurationapi.go github.com/coltoneshaw/mattermost-plugin-customers/server/sqlstore ConfigurationAPI
 endif
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
